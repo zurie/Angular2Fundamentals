9a541647bf3aed1fa80d396b849d0dbcebe078e0
Wael Kdouh <waekdo@microsoft.com>
2016-02-22 21:58:25 -0800

    Say Hello World to Angular 2

---
+++ app/app.ts
@@ -0,0 +1,11 @@
+import {Component} from 'angular2/core';
+import {bootstrap} from 'angular2/platform/browser';
+
+
+@Component({
+    selector: 'app',
+    template: '<h1>Hello World'
+})
+export class AppComponent { }
+
+bootstrap(AppComponent);
